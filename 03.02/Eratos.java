51e348651417eba3a55b2d5266f0a4d900bc59d6
- <dygks3240@hanyang.ac.kr>
2021-03-02 14:19:38 +0000

    03.02

---
+++ 03.02/Eratos.java
@@ -0,0 +1,11 @@
+package project;
+import java.util.*;
+
+public class Eratos{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        ArrayList<Boolean> arr = new ArrayList<Boolean>();
+    }
+}
