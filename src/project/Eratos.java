32df56477d1034b6ba3e636dd174e45f0fa74a6a
- <dygks3240@hanyang.ac.kr>
2021-03-03 10:22:05 +0000

    03.03

--- src/project/Eratos.java
+++ src/project/Eratos.java
@@ -7,5 +7,27 @@ public class Eratos{
         int n = sc.nextInt();
         int k = sc.nextInt();
         ArrayList<Boolean> arr = new ArrayList<Boolean>();
+        int res = 0;
+        
+        arr.add(false);
+        for(int i=1;i<n;i++){
+            arr.add(i,true);
+        }
+        for(int i=2;i<n;i++){
+                for(int j=i;j<n;j+=i){
+                    if(!arr.get(i))
+                        continue;
+                    arr.set(j,false);
+                    res++;
+                    if(res == k){
+                        System.out.println(j);
+                        System.exit(0);
+                    }
+                    
+                }
+            
+        }
+        
+        
     }
 }
