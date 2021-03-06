3f58ebdcd66ef4a150488eafe3ff0d32f3dd280b
- <dygks3240@hanyang.ac.kr>
2021-03-06 13:18:12 +0000

    03.06

--- 03.04/gwalho.java
+++ 03.04/gwalho.java
@@ -16,7 +16,7 @@ public class gwalho{
     static String VPS(String s){
          Stack<Character> stk = new Stack<>();
          for(int i =0;i<s.length();i++){
-             char c = s.charAt(i);
+             char c = s.charAt(i); 
              if(c == '('){
                  stk.push(c);
              }
