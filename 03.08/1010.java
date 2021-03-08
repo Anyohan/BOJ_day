38a52252b20fcaabfb4565f0b9397e47b8d5b8f4
- <dygks3240@hanyang.ac.kr>
2021-03-08 13:22:36 +0000

    03.08

---
+++ 03.08/1010.java
@@ -0,0 +1,31 @@
+package project;
+import java.util.*;
+
+public class bridge{
+    public static void main(String[] args) {
+        int [][] com = new int[30][30];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<30;i++){
+            com[i][i] = 1;
+            com[i][0] = 1;
+        }
+        
+        for(int i = 2;i<30;i++){
+            for(int j = 1;j<30;j++){
+                com[i][j] = com[i-1][j-1] + com[i-1][j]; 
+            }
+        }
+        
+        for(int i =0;i<n;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sb.append(com[b][a]).append("\n");
+        }
+        System.out.println(sb);
+         
+    }
+    
+}
