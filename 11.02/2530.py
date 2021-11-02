bfcbd1acd682e459c6042df9b6733a8d9667f54b
Anyohan <dygks3240@hanyang.ac.kr>
2021-11-02 15:40:42 +0900

    11.02

---
+++ 11.02/2530.py
@@ -0,0 +1,49 @@
+#내 풀이
+
+a,b,c = map(int,input().split())
+d = int(input())
+
+m = (c+d)//60
+s = (c+d)%60
+r = b+m
+g = r//60
+k = a+g%24
+ 
+
+if k >= 24:
+    if r >= 60:
+        print(k-24,r%60,s)
+    else:
+        print(k-24,r,s)
+else:
+    if r >= 60:
+        print(k,r%60,s)
+    else:
+        print(k,r,s)
+
+
+# 다른풀이
+
+hour, minute, second = map(int, input().split())
+cook = int(input())
+ 
+second += cook % 60
+cook = cook // 60
+ 
+if second >= 60:
+    second -= 60
+    minute += 1
+ 
+minute += cook % 60
+cook = cook // 60
+ 
+if minute >= 60:
+    minute -= 60
+    hour += 1
+ 
+hour += cook % 24
+if hour > 23:
+    hour -= 24
+ 
+print(hour, minute, second)
+ 
