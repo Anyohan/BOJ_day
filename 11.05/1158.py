e473629a3da51e967d49f9d89d577e599145c063
Anyohan <dygks3240@hanyang.ac.kr>
2021-11-05 16:11:52 +0900

    11.05

---
+++ 11.05/1158.py
@@ -0,0 +1,25 @@
+N,K = map(int,input().split())
+caseList = list(range(1,N+1))
+josephList = []
+head = 0
+
+for i in range(N):
+    head += K-1
+    if len(caseList) <= head:
+        head = head%len(caseList)
+    josephList.append(caseList.pop(head))
+print("<",", ".join(str(i) for i in josephList),">",sep = "")
+
+#head = head%len(caseList)
+#[1,2,4,5,7] head = 6, len = 5
+#6번째 사람을 구해야되는데 5명밖에 없다
+#그러면... 6%5 는 1 이니까 다시 앞으로
+#1
+
+
+
+    
+    
+
+    
+
